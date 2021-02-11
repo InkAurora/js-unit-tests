919beb0fe73618578caa352e991c7b81de0aad73
InkAurora <marthus2001@gmail.com>
2021-02-11 16:21:41 -0300

    Initial commit

---
+++ src/objCalculator.js
@@ -0,0 +1,38 @@
+/*
+  Desenvolva um objeto calculator que possui quatro chaves:
+    - add;
+    - mult;
+    - div;
+    - sub.
+  Para cada uma delas atribua uma função que realiza a respectiva operação.
+  A função deve receber dois inteiros e retornar um inteiro.
+  Os resultados das divisões devem sempre ser arredondados para baixo.
+
+  Faça as funções com arrow functions!
+
+  Parâmetros:
+  - Um número inteiro;
+  - Um número inteiro;
+
+  Comportamento:
+  calculator.add(1, 1) // Retorno: 2;
+  calculator.div(3, 2) // Retorno: 1;
+*/
+
+function divide(num1, num2) {
+  let result;
+  result = Math.round(num1 / num2);
+  if (num1 / num2 < result) {
+    result -= 1;
+  }
+  return result;
+}
+
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => divide(num1, num2),
+  sub: (num1, num2) => num1 - num2,
+};
+
+module.exports = calculator;
---
+++ tests/productDetails.spec.js
@@ -0,0 +1,57 @@
+/* eslint-disable max-len*/
+/* eslint-disable no-unused-vars */
+
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+const { AssertionError } = require('assert');
+
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+describe('#productDetails', () => {
+  it('tests the function has the correct behaviour', () => {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('asd', 'dsa')), true);
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('asd', 'dsa').length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('asd', 'dsa')[0], 'object');
+    assert.strictEqual(typeof productDetails('asd', 'dsa')[1], 'object');
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('asd', 'dsa')[0], productDetails('asd', 'dsa')[1]);
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    const productId1 = Object.values(Object.values(Object.values(productDetails('asd', 'dsa'))[0])[1])[0];
+    const productId2 = Object.values(Object.values(Object.values(productDetails('asd', 'dsa'))[1])[1])[0];
+    assert.strictEqual(productId1[productId1.length - 1], "3");
+    assert.strictEqual(productId1[productId1.length - 2], "2");
+    assert.strictEqual(productId1[productId1.length - 3], "1");
+    assert.strictEqual(productId1[productId2.length - 1], "3");
+    assert.strictEqual(productId1[productId2.length - 2], "2");
+    assert.strictEqual(productId1[productId2.length - 3], "1");
+  });
+});
